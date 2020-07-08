9bdbf86a8f0ba2b5c0acabee46038b61b147845a
steve <stevepesce879@gmail.com>
2020-07-08 17:44:53 -0400

    fixed typo in module, moved module to lib, added config/application line for loading lib files

--- app/models/deposit_account.rb
+++ app/models/deposit_account.rb
@@ -1,7 +1,7 @@
 class DepositAccount < ApplicationRecord
   before_create :generate_account_number
   belongs_to :online_banking_account
-  includes AccountModule
+  include AccountModule
   #extracted from bank, since DA belongs to OBA, which belongs to Bank 
   def routing_number
     self.online_banking_account.bank.routing_number
@@ -10,7 +10,10 @@ class DepositAccount < ApplicationRecord
     self.created_at
   end
 
-  private
+  def test
+    self.status
+  end
+  
   #set account number before creation
   #TODO: Validate that it hasnt been used
   def generate_account_number
