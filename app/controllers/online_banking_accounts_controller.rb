8bd079baea6d07c8a67fd8ebb5b0bcd957e879e6
steve <stevepesce879@gmail.com>
2020-07-08 18:22:40 -0400

    finished skateboard, created hook that deposits Loan#amount to DepositAccount#amount (checking) added comments

--- app/controllers/online_banking_accounts_controller.rb
+++ app/controllers/online_banking_accounts_controller.rb
@@ -1,6 +1,7 @@
 class OnlineBankingAccountsController < ApplicationController
-  
-
+  Delete Me
+  TODO: make login functionality & validate that user has checking account before making loan
+  TODO: make way for user to pay their loan
   before_action :set_oba, only: [:show,:new_checking_account,:new_savings_account,:new_loan,:create_loan]
   def new      
     @oba = OnlineBankingAccount.new
