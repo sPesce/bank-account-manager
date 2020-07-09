b1018d8ad3685a1796134f9de5da113fb3fc78f4
steve <stevepesce879@gmail.com>
2020-07-09 12:27:44 -0400

    added password_digest to users, started with secure password implementation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_08_200310) do
+ActiveRecord::Schema.define(version: 2020_07_09_162630) do
 
   create_table "banks", force: :cascade do |t|
     t.string "name"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2020_07_08_200310) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "password_digest"
   end
 
 end
