f9e8e1a0e1e85874eb7b5982cff8aea0a1d65f52
steve <stevepesce879@gmail.com>
2020-07-09 13:16:11 -0400

    app requires secure password, created login functionalit @ localhost:3000/, user id now stored in session[:user_id]

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,17 @@
 class SessionsController < ApplicationController
   def new
-      
+      @user = User.new
   end
 
   def create
-      session[:username] = params[:username]
-      redirect_to '/'
+    @user = User.find_by(username: user_params[:username])
+    
+    return head(:forbidden) unless user_params[:password] == @user.password
+    session[:user_id] = @user.id
+    redirect_to home_path(session[:user_id])
   end
+  private
+  def user_params
+    params.require(:user).permit(:username,:password)
+  end  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_09_162630) do
+ActiveRecord::Schema.define(version: 2020_07_09_170911) do
 
   create_table "banks", force: :cascade do |t|
     t.string "name"
@@ -52,7 +52,6 @@ ActiveRecord::Schema.define(version: 2020_07_09_162630) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
